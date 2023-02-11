75ecd3c69dbbf395d8efe198cda7f700956c661e
www.rubiluqman7@gmail.com <www.rubiluqman7@gmail.com>
2023-02-12 01:17:54 +0300

    completed

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
+ def countdown num
+    while num >=1
+        puts "#{num} SECOND(S)!"
+        countdown_with_sleep()
+        num -=1
+    end
+    "HAPPY NEW YEAR!"
+    end
 
-def countdown
+def countdown_with_sleep(seconds =5)
+    sleep(seconds)
 end
